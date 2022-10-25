03b29a4d82db8dbdf71e91f1b0ada21ebaa1f4a4
Tobibito-dev <tobibito.dev@gmail.com>
2022-10-25 20:04:14 +0200

    add existing data

---
+++ data_updater.py
@@ -0,0 +1,21 @@
+import os
+
+
+# check if genshin_data exists
+def check_data():
+    if os.path.isdir('./genshin_data/.git/'):
+        print("updating genshin genshin_data_manager")
+        update_data()
+    else:
+        print("genshin_data missing")
+        get_data()
+
+
+# clone dimbreath genshin_data repo
+def get_data():
+    os.system('git clone --progress https://github.com/Dimbreath/GenshinData.git genshin_data --depth 1')
+
+
+# pull dimbreath genshin_data repo
+def update_data():
+    os.system('cd ./genshin_data/ && git fetch --progress --depth 1 && git reset --hard origin/master')
---
+++ map/characters.py
@@ -0,0 +1,73 @@
+char = {
+    # albedo,
+    # amber,
+    # barbara,
+    # beidou,
+    # bennett,
+    # chongyun,
+    # diluc,
+    # diona,
+    "eula": {
+        "skill": {
+            "brand_dmg": [3765549071, "skillParam"],
+            "grimheart_duration": [164817062, "skillParam"],
+        },
+    },
+    # fischl,
+    # ganyu,
+    # hutao,
+    # jean,
+    "kaedehara_kazuha": {
+        "burst": {
+            "name": 2015878197,
+            "description": [1543060392, "paragraph"],
+            "slash_dmg": [439659606, "skillParam"],
+        },
+    },
+    # kaeya,
+    "kamisato_ayaka": {
+        "burst": {
+            "cutting": [373269062, "skillParam"],
+            "bloom": [2543020158, "skillParam"],
+        }
+    },
+    # kamisato_ayato,
+    # keqing,
+    # klee,
+    # lisa,
+    # mona,
+    # ningguang,
+    "noelle": {
+        "skill": {
+            "trigger_chance": [3971383039, "skillParam"],
+        },
+        "burst": {
+            "atkBonus": [4151293863, "skillParam"],
+        },
+    },
+    # qiqi,
+    # razor;
+    # rosaria,
+    # sucrose,
+    # tartaglia,
+    "traveler": {
+        "electro": {
+            "skill": {
+                "ener_regen": [2165261751, "skillParam"],
+                "ener_rech_inc": [3328414367, "skillParam"],
+            },
+            "burst": {
+                "thunder_dmg": [3560985918, "skillParam"],
+            },
+        },
+    },
+    # venti,
+    # xiao,
+    # xiangling,
+    # xingqiu,
+    # xinyan,
+    # yanfei,
+    "zhongli": {
+        "constellation_name": 2721221067
+    }
+}
