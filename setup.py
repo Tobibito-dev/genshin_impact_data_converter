bda8b0079048a9803792ce23556c011e487f4f67
Tobibito-dev <tobibito.dev@gmail.com>
2022-10-25 21:29:37 +0200

    add src folder

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gidm',
+    version='1.0.0',
+    license='MIT',
+    author='Tobibito-dev',
+    author_email='tobibito.dev@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'', 'src'},
+    url='https://github.com/Tobibito-dev/genshin_impact_data_manager',
+    keywords='gidm',
+    install_requires=['setuptools']
+)
