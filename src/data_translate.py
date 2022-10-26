503470d342caffa5ed7871beecdec3bf01979714
Tobibito-dev <tobibito.dev@gmail.com>
2022-10-26 15:02:02 +0200

    change data update method

--- src/data_translate.py
+++
@@ -1,6 +0,0 @@
-import os
-
-from data_translator import util
-import data_translator
-
-# util.dump_file('GenshinData/ExcelBinOutput/WorldLevelExcelConfigData.json', 'data/test.json')
--- src/data_update.py
+++ src/data_update.py
@@ -1,5 +1,20 @@
 import data_paths
 
 import data_translator
+import data_updater
 
-data_translator.translate.translate_data(data_paths.genshin_data_path)
+
+# pull data then convert
+def update_data():
+    pull_data()
+    convert_data()
+
+
+def pull_data():
+    data_updater.pull.check_data(data_paths.genshin_data_path)
+
+
+def convert_data():
+    pass
+
+update_data()
