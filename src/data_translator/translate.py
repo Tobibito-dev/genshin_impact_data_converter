c1e5e92b7dfe31130f6df12e073b524a1fe853e9
Tobibito-dev <tobibito.dev@gmail.com>
2022-11-05 22:46:15 +0100

    add pprint for testing

--- src/data_translator/translate.py
+++ src/data_translator/translate.py
@@ -1,3 +1,5 @@
+import pprint
+
 from . import util
 from . import convert_template
 
@@ -8,4 +10,7 @@ def translate_data(genshin_data_path, language_keys: list[str]):
     languages = util.text_map_util.get_languages(genshin_data_path, language_keys)
 
     weapon = convert_template.convert_template_data(genshin_data_path, languages, templates.weapon.template)
-    print(weapon['primordial_jade_winged_spear'])
+
+    jade_spear = weapon['primordial_jade_winged_spear']
+    print(jade_spear)
+    # pprint.pprint(jade_spear)
