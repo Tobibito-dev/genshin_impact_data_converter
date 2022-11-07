b1e92479d95c7c153b9df05946684fc48c96d368
Tobibito-dev <tobibito.dev@gmail.com>
2022-11-07 12:07:25 +0100

    add missing paths

--- src/data_translator/translate.py
+++ src/data_translator/translate.py
@@ -8,9 +8,9 @@ def translate_data(genshin_data_path, language_keys: list[str]):
     languages = text_map_util.get_languages(genshin_data_path, language_keys)
 
     translated_data = {
-        'artifact': '',
-        'character': '',
-        'common': '',
+        'artifact': convert_template.convert_template_data(genshin_data_path, languages, templates.artifact.template),
+        'character': convert_template.convert_template_data(genshin_data_path, languages, templates.character.template),
+        'common': convert_template.convert_template_data(genshin_data_path, languages, templates.common.template),
         'food': convert_template.convert_template_data(genshin_data_path, languages, templates.food.template),
         'material': convert_template.convert_template_data(genshin_data_path, languages, templates.material.template),
         'weapon': convert_template.convert_template_data(genshin_data_path, languages, templates.weapon.template)
