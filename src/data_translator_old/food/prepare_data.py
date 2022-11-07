8c3a6aec19e03de316369bf385a5a375190310e7
Tobibito-dev <tobibito.dev@gmail.com>
2022-11-07 11:43:01 +0100

    remove data translator old

--- src/data_translator_old/food/prepare_data.py
+++
@@ -1,8 +0,0 @@
-import json
-
-from .. import config_data_paths
-
-
-def food_data(genshin_data_path):
-    temp_list = json.loads(open(genshin_data_path + config_data_paths.food_path).read())
-    return temp_list
--- src/data_translator_old/util/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import float_calculator
-from . import text_map_util
