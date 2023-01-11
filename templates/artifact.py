4d3a68a4892ed594115628ae82430766ffe29c82
Tobibito-dev <tobibito.dev@gmail.com>
2023-01-11 15:22:14 +0100

    get other artifact values

--- templates/artifact.py
+++ templates/artifact.py
@@ -10,20 +10,31 @@ template = {
     "templateType": "artifact",
     "id": "$suitId",
     "name": "$suitId/display_item/.param/#0/nameTextMapHash/text_map",
+    "icon": "$suitId/display_item/.param/#0/icon",
 
     "flower": {
         "name": "$flowerId/artifact/.id/nameTextMapHash/text_map",
+        "id": "$flowerId",
+        "icon": "$flowerId/artifact/.id/icon",
     },
     "feather": {
         "name": "$leatherId/artifact/.id/nameTextMapHash/text_map",
+        "id": "$leatherId",
+        "icon": "$leatherId/artifact/.id/icon",
     },
     "sands": {
         "name": "$sandId/artifact/.id/nameTextMapHash/text_map",
+        "id": "$sandId",
+        "icon": "$sandId/artifact/.id/icon",
     },
     "goblet": {
         "name": "$cupId/artifact/.id/nameTextMapHash/text_map",
+        "id": "$cupId",
+        "icon": "$cupId/artifact/.id/icon",
     },
     "circlet": {
         "name": "$capId/artifact/.id/nameTextMapHash/text_map",
+        "id": "$capId",
+        "icon": "$capId/artifact/.id/icon",
     },
 }
